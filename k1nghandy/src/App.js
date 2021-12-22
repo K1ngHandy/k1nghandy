19c1b68cfceff08e4d8b1c8b6da10596d8d85502
Stephen-Handy <a.king.life@pm.me>
2021-12-21 20:13:14 -0500

    routes

--- k1nghandy/src/App.js
+++ k1nghandy/src/App.js
@@ -1,4 +1,6 @@
 import './App.css';
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
 
@@ -6,6 +8,10 @@ function App() {
     <div className="App">
       <h1>Welcome!</h1>
       <h2>Website Building in Progress, Stay Tuned.</h2>
+      <Routes>
+        <Route exact path="/" />
+        <Route path="Social" />
+      </Routes>
     </div>
   );
 }
