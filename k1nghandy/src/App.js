6904a0b790eaaf68c580160f98f1cc5e195843a3
Stephen-Handy <a.king.life@pm.me>
2022-01-03 14:16:51 -0500

    routing

--- k1nghandy/src/App.js
+++ k1nghandy/src/App.js
@@ -1,28 +1,41 @@
 import './App.css';
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './components/Home';
-import Weather from './components/Weather';
+import About from './components/About';
 
 function App() {
   const pic = 'https://images.pexels.com/photos/48770/business-time-clock-clocks-48770.jpeg?cs=srgb&dl=pexels-pixabay-48770.jpg&fm=jpg://www.pexels.com/photo/london-new-york-tokyo-and-moscow-clocks-48770/';
 
   return (
     <div className="App">
-      <br />
+      <Router>
+        <div>
+          <nav className='nav'>
+            <ul>
+              <li>
+                <Link to='/'>Home</Link>
+              </li>
+              <li>
+                <Link to='/about'>About</Link>
+              </li>
+            </ul>
+          </nav>
+
+          <Routes>
+            <Route exact path="/" component={Home} />
+            {/* <Home /> */}
+            <Route path="/about" component={About} />
+            {/* <About /> */}
+          </Routes>
+        </div>
+      </Router>
+
       {/* <Home /> */}
       <h1>Website Building in Progress</h1>
       <img class='App-logo' src={pic} alt='london-new-york-tokyo-and-moscow-clocks' />
-      <h2>Stay Tuned. . .</h2>
-      <button class='button'>Enter</button>
       <br />
       <br />
-      <br />
-      
-      <Routes>
-        <Route exact path="/" component={Home} />
-        <Route exact path="Weather" component={Weather} />
-      </Routes>
     </div>
   );
 }
---
+++ k1nghandy/src/components/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const About = () => {
+    return (
+        <div>
+            <h2>Stay Tuned. . .</h2>
+            <button class='button'>Enter</button>
+        </div>
+    )
+}
+
+export default About;
